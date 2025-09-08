c764558c38b072357a0d6ca9a834f8d8e971e26f
Kateryna <marusia.lukash@gmail.com>
2025-09-08 10:32:47 +0300

    Добавлено приветствие в консоль

--- Program.cs
+++ Program.cs
@@ -1,10 +1,1 @@
-﻿using System;
-
-class Program
-{
-    static void Main()
-    {
-        Console.WriteLine("Моё имя: Катерина");
-        Console.WriteLine("Моя группа: 3-022");
-    }
-}
+﻿Console.WriteLine("Привіт, GitHub!");
